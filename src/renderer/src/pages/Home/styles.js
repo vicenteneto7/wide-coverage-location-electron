be4283f5ff2ad05973fdd94063f1932d691d5eb2
Vicente <vicentefmm2019@gmail.com>
2024-03-15 11:22:15 -0300

    style: estilização de divs e botão

---
+++ src/renderer/src/pages/Home/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export const Section = styled.div`
+    width: 100%;
+    height: 5rem;
+    display: flex;
+`
+
+export const Button = styled.button`
+    width: 9.1rem;
+    height: 3.1rem;
+    background-color: #4CB538;
+    color: white;
+    border-radius: 1rem;
+`
